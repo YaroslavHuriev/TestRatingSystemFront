fce4387914c18040322ab7a31e3a3cf0e8e4a551
Ярослав Гур'єв <jarikhuruev@gmail.com>
2021-11-25 22:55:51 +0200

    - додано функціонал виходу з акаунта - додано авторизацію адміністратора -додано сторінку створення завдання - додано сторінку вибору завдань для кандидата

---
+++ src/components/taskChoice.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<div v-for="task in tasks" v-bind:key="task.id">
+			<router-link v-bind:to="'/Submission/Add/'+task.id">{{task.title}}</router-link>
+		</div>
+	</div>
+</template>
+
+<script>
+import api from "@/api";
+
+export default {
+	name: "taskChoice",
+	data() {
+		return {
+			tasks: []
+		}
+	},
+	mounted() {
+		api.get('TaskChoosing/Tasks')
+			.then(response => this.tasks = response.data)
+			.catch(err => console.log(err));
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
